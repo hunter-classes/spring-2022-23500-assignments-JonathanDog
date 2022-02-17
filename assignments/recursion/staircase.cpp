01dc2692298148733e2633345b26a548b512d538
JonathanDog <rangersps42@gmail.com>
2022-02-17 10:19:16 -0500

    staircase.cpp

--- assignments/recursion/staircase.cpp
+++ assignments/recursion/staircase.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 
 int arr[] = {1,2,3};
+int count[100];
+
+
+
 
 int countSteps(int n ) {
     if (n == 0) {
         return 1;
     }
     int total = 0;
+    if(count[n] != 0) {
+   	return count[n];	
+    }
     for (int i = 0; i<3; i++) {
         if(n - arr[i] >= 0) {
-            total += countSteps(n - arr[i]);
+	    int t = countSteps(n - arr[i]);
+            total += t;
+	    count[n - arr[i]] = t; 
         }
     }
     return total;
@@ -27,8 +36,8 @@ int main()
    std::cout << "5 steps: " ;
    std::cout << countSteps(5) << std::endl;
    
-   std::cout << "6 steps: " ;
-   std::cout << countSteps(6) << std::endl;
+   std::cout << "35 steps: " ;
+   std::cout << countSteps(35) << std::endl;
 
     return 0;
-}
+
